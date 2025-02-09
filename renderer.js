17a0128ead35c138af010e62448e1f95e8ebb7d9
marinkres <131777129+marinkres@users.noreply.github.com>
2025-02-09 21:19:51 +0100

    add functionality to dynamically spawn new cats and change background; update HTML structure and styles

--- renderer.js
+++ renderer.js
@@ -1,5 +1,8 @@
 const minimizeBtn = document.getElementById("minimize-btn")
 const closeBtn = document.getElementById("close-btn")
+const feedBtn = document.getElementById("feed-btn")
+const backgroundContainer = document.getElementById("background-container")
+const catsContainer = document.querySelector(".cats-container")
 
 minimizeBtn.addEventListener("click", () => {
   window.electronAPI.minimizeWindow()
@@ -9,6 +12,32 @@ closeBtn.addEventListener("click", () => {
   window.electronAPI.closeWindow()
 })
 
+// Add event listener for the feed button
+feedBtn.addEventListener("click", () => {
+  changeBackground("assets/background2.png")
+  spawnNewCat()
+})
+
+// Function to change the background
+function changeBackground(newBackgroundPath) {
+  backgroundContainer.style.backgroundImage = `url('${newBackgroundPath}')`
+}
+
+// Function to spawn a new cat
+function spawnNewCat() {
+  if (!document.querySelector(".new-cat")) {
+    const newCatContainer = document.createElement("div")
+    newCatContainer.className = "cat-container"
+    const newCat = document.createElement("img")
+    newCat.className = "new-cat"
+    newCat.src = "assets/new-cat-frame1.png"
+    newCat.alt = "New Pixel Cat"
+    newCatContainer.appendChild(newCat)
+    catsContainer.appendChild(newCatContainer)
+    animateNewCat(newCat)
+  }
+}
+
 const cat = document.getElementById("cat")
 const frames = [
   "assets/pixil-frame-0.png",
@@ -16,16 +45,33 @@ const frames = [
   "assets/pixil-frame-2.png",
   "assets/pixil-frame-3.png",
   "assets/pixil-frame-4.png",
-  // Add more frames as needed
 ]
-
+const newCatFrames = [
+  "assets/marin1.png",
+  "assets/marin2.png",
+  "assets/marin3.png",
+  "assets/marin4.png",
+  "assets/marin5.png",
+]
 let currentFrame = 0
-const frameRate = 5 // Adjust as needed
+const frameRate = 200 // Time (in milliseconds) between frames
 
 function animateCat() {
   cat.src = frames[currentFrame]
   currentFrame = (currentFrame + 1) % frames.length
+  setTimeout(animateCat, frameRate)
 }
 
-setInterval(animateCat, 1000 / frameRate)
+function animateNewCat(newCat) {
+  let newCatFrame = 0
+  function animate() {
+    newCat.src = newCatFrames[newCatFrame]
+    newCatFrame = (newCatFrame + 1) % newCatFrames.length
+    setTimeout(animate, frameRate)
+  }
+  animate()
+}
+
+// Start the animation
+animateCat()
 
